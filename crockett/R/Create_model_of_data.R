5d487d355d57e490071484b14cdb7fe893d44c31
tessera.crockett@selu.edu <tessera.crockett@selu.edu>
2020-10-19 20:44:39 +0000

    add function for project two update comp notebook

---
+++ crockett/R/Create_model_of_data.R
@@ -0,0 +1,10 @@
+#' Create model of data with biomass as response to flooding as predictor
+#' 
+#' @param lm Linear Model(response~predictor, data = )(catagorical, no default)
+#' @return model_fit
+#' 
+#' @example 
+#' lm(biomass~flooding, data =plant_data)
+#' 
+
+  
